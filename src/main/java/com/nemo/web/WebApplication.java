62602aec3525a5848c0c9a672b67d7b85fe7e9f1
MINH2201 <thaiminhnguyen2003@gmail.com>
2024-03-27 23:33:23 +0700

    add static web resource

--- src/main/java/com/nemo/web/WebApplication.java
+++ src/main/java/com/nemo/web/WebApplication.java
@@ -2,12 +2,11 @@ package com.nemo.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class WebApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(WebApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
 }
