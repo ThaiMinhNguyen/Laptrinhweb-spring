2f01bad35a447a00de542fb7350b97242a253f0b
Duong-Nguyen-Ngoc-Hai <156879321+Duong-Nguyen-Ngoc-Hai@users.noreply.github.com>
2024-04-08 15:34:10 +0700

    Update ExcelUtil.java

--- src/main/java/com/nemo/web/util/ExcelUtil.java
+++ src/main/java/com/nemo/web/util/ExcelUtil.java
@@ -1,41 +1,41 @@
-//package com.nemo.web.util;
-//
-//import org.apache.poi.ss.usermodel.*;
-//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-//import com.nemo.web.exam.Question;
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.util.ArrayList;
-//import java.util.Iterator;
-//import java.util.List;
-//
-//public class ExcelUtil {
-//
-//    public static List<Question> readQuestionsFromExcel(InputStream inputStream) throws IOException {
-//        List<Question> questions = new ArrayList<>();
-//
-//        Workbook workbook = new XSSFWorkbook(inputStream);
-//        Sheet sheet = workbook.getSheetAt(0);
-//
-//        Iterator<Row> rowIterator = sheet.iterator();
-//        while (rowIterator.hasNext()) {
-//            Row row = rowIterator.next();
-//
-//            Cell questionCell = row.getCell(0);
-//            Cell answerCell = row.getCell(1);
-//
-//            String questionText = questionCell.getStringCellValue();
-//            String correctAnswer = answerCell.getStringCellValue();
-//
-//            Question question = new Question();
-//            question.setQuestionText(questionText);
-//            question.setCorrectAnswer(correctAnswer);
-//
-//            questions.add(question);
-//        }
-//
-//        workbook.close();
-//
-//        return questions;
-//    }
-//}
+package com.nemo.web.util;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import com.nemo.web.exam.Question;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ExcelUtil {
+
+    public static List<Question> readQuestionsFromExcel(InputStream inputStream) throws IOException {
+        List<Question> questions = new ArrayList<>();
+
+        Workbook workbook = new XSSFWorkbook(inputStream);
+        Sheet sheet = workbook.getSheetAt(0);
+
+        Iterator<Row> rowIterator = sheet.iterator();
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+
+            Cell questionCell = row.getCell(0);
+            Cell answerCell = row.getCell(1);
+
+            String questionText = questionCell.getStringCellValue();
+            String correctAnswer = answerCell.getStringCellValue();
+
+            Question question = new Question();
+            question.setQuestionText(questionText);
+            question.setCorrectAnswer(correctAnswer);
+
+            questions.add(question);
+        }
+
+        workbook.close();
+
+        return questions;
+    }
+}
