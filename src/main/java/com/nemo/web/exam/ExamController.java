dc9113c2a94f10b6a2a34b1c77f0fc435ff0aaf0
Duong-Nguyen-Ngoc-Hai <156879321+Duong-Nguyen-Ngoc-Hai@users.noreply.github.com>
2024-04-08 15:31:35 +0700

    Update ExamController.java

--- src/main/java/com/nemo/web/exam/ExamController.java
+++ src/main/java/com/nemo/web/exam/ExamController.java
@@ -2,7 +2,8 @@ package com.nemo.web.exam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import com.nemo.web.exception.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
@@ -13,25 +14,48 @@ public class ExamController {
     @Autowired
     private ExamRepository examRepository;
 
+    public ExamController(ExamRepository examRepository) {
+        this.examRepository = examRepository;
+    }
     @GetMapping
     public List<Exam> getAllExams() {
         return examRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Exam> getExamById(@PathVariable Long id) {
+        Exam exam = examRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Exam not found with id: " + id));
+
+        return ResponseEntity.ok(exam);
+    }
+
     @PostMapping
-    public Exam createExam(@RequestBody Exam exam) {
-        return examRepository.save(exam);
+    public ResponseEntity<Exam> createExam(@RequestBody Exam exam) {
+        Exam createdExam = examRepository.save(exam);
+        return ResponseEntity.ok(createdExam);
     }
 
     @PutMapping("/{id}")
-    public Exam updateExam(@PathVariable Long id, @RequestBody Exam exam) {
-        exam.setId(id);
-        return examRepository.save(exam);
+    public ResponseEntity<Exam> updateExam(@PathVariable Long id, @RequestBody Exam updatedExam) {
+        Exam exam = examRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Exam not found with id: " + id));
+
+        exam.setName(updatedExam.getName());
+        exam.setDescription(updatedExam.getDescription());
+        exam.setTestType(updatedExam.getTestType());
+        exam.setQuestions(updatedExam.getQuestions());
+
+        Exam updatedExamEntity = examRepository.save(exam);
+        return ResponseEntity.ok(updatedExamEntity);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteExam(@PathVariable Long id) {
-        examRepository.deleteById(id);
+    public ResponseEntity<Void> deleteExam(@PathVariable Long id) {
+        Exam exam = examRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Exam not found with id: " + id));
+
+        examRepository.delete(exam);
+        return ResponseEntity.noContent().build();
     }
 }
-
