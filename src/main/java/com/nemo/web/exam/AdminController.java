26768595ec4ea378790529dd88f4d1cefc3130a6
Duong-Nguyen-Ngoc-Hai <156879321+Duong-Nguyen-Ngoc-Hai@users.noreply.github.com>
2024-04-08 15:32:35 +0700

    Update AdminController.java

--- src/main/java/com/nemo/web/exam/AdminController.java
+++ src/main/java/com/nemo/web/exam/AdminController.java
@@ -1,46 +1,46 @@
-//package com.nemo.web.exam;
-//
-//import com.nemo.web.exception.ResourceNotFoundException;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/api/admin")
-//public class AdminController {
-//    @Autowired
-//    private final ExamRepository examRepository;
-//
-//    
-//    public AdminController(ExamRepository examRepository) {
-//        this.examRepository = examRepository;
-//    }
-//
-//    @PostMapping("/exams")
-//    public ResponseEntity<Exam> createExam(@RequestBody Exam exam) {
-//        Exam createdExam = examRepository.save(exam);
-//        return ResponseEntity.ok(createdExam);
-//    }
-//
-//    @PutMapping("/exams/{id}")
-//    public ResponseEntity<Exam> updateExam(@PathVariable Long id, @RequestBody Exam updatedExam) {
-//        Exam exam = examRepository.findById(id)
-//                .orElseThrow(() -> new ResourceNotFoundException("Exam not found with id: " + id));
-//
-//        exam.setName(updatedExam.getName());
-//        exam.setDescription(updatedExam.getDescription());
-//        exam.setTestType(updatedExam.getTestType());
-//
-//        Exam updatedExamEntity = examRepository.save(exam);
-//        return ResponseEntity.ok(updatedExamEntity);
-//    }
-//
-//    @DeleteMapping("/exams/{id}")
-//    public ResponseEntity<Void> deleteExam(@PathVariable Long id) {
-//        Exam exam = examRepository.findById(id)
-//                .orElseThrow(() -> new ResourceNotFoundException("Exam not found with id: " + id));
-//
-//        examRepository.delete(exam);
-//        return ResponseEntity.noContent().build();
-//    }
-//}
+package com.nemo.web.exam;
+
+import com.nemo.web.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+    @Autowired
+    private final ExamRepository examRepository;
+
+    
+    public AdminController(ExamRepository examRepository) {
+        this.examRepository = examRepository;
+    }
+
+    @PostMapping("/exams")
+    public ResponseEntity<Exam> createExam(@RequestBody Exam exam) {
+        Exam createdExam = examRepository.save(exam);
+        return ResponseEntity.ok(createdExam);
+    }
+
+    @PutMapping("/exams/{id}")
+    public ResponseEntity<Exam> updateExam(@PathVariable Long id, @RequestBody Exam updatedExam) {
+        Exam exam = examRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Exam not found with id: " + id));
+
+        exam.setName(updatedExam.getName());
+        exam.setDescription(updatedExam.getDescription());
+        exam.setTestType(updatedExam.getTestType());
+
+        Exam updatedExamEntity = examRepository.save(exam);
+        return ResponseEntity.ok(updatedExamEntity);
+    }
+
+    @DeleteMapping("/exams/{id}")
+    public ResponseEntity<Void> deleteExam(@PathVariable Long id) {
+        Exam exam = examRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Exam not found with id: " + id));
+
+        examRepository.delete(exam);
+        return ResponseEntity.noContent().build();
+    }
+}
